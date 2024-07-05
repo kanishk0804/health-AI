7d21eee198151dfcbdf6ca9a313a044f495dafcb
kanishk0804 <kanishkchaturvedi@gmail.com>
2024-07-06 00:41:24 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const publicUrl = "https://your-ngrok-url";
+    const publicUrl = "https://e4a0-35-201-174-116.ngrok-free.app";
 
     document.querySelectorAll('.tab').forEach(tab => {
         tab.addEventListener('click', function() {
